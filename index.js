03ed5fb7f0972cf6638611f43f7e66f3ce685aec
Mayank Badola <badola21295@gmail.com>
2016-02-16 20:00:35 +0530

    better code

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-/*
-module.exports = function (string) {
-  return /^[a-zA-Z\.,\-() \n\r]*$/.test(string);
-};
-*/
-module.exports = {
-  devCode: (string) => {
-    return /^[a-zA-Z0-9!"#$%&'()*+,.\\\/:;<=>?@\[\] ^_`{|}~-\s\r\n\t]+$/.test(string);
-  },
-  upAlpha: (string) => {
-    return /^[A-Z]+$/.test(string);
-  },
-  lowAlpha: (string) => {
-    return /^[a-z]+$/.test(string);
-  },
-  digits: (string) => {
-    return /^[0-9]+$/.test(string);
-  },
-  alphaNum: (string) => {
-    return /^[a-zA-z0-9]+$/.test(string);
-  },
-  enSentence: (string) => {
-    return /^[a-zA-z.,;'"&\s]+$/.test(string);
+module.exports = (string,type) => {
+  const regexs = {
+    devCode: /^[a-zA-Z0-9!"#$%&'()*+,.\\\/:;<=>?@\[\] ^_`{|}~-\s\r\n\t]+$/,
+    upAlpha: /^[A-Z]+$/,
+    lowAlpha: /^[a-z]+$/,
+    digits: /^[0-9]+$/,
+    alphaNum: /^[a-zA-z0-9]+$/,
+    enSentence: /^[a-zA-z.,;'"&\s]+$/
   }
 }
